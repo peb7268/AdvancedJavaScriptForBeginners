d9e1a1139a49cd914b16f8f3acea8d571438b326
Randall Prince <randallprince@the-wizard.local>
2020-07-06 23:51:06 -0400

    created new engine model class type, added weight to cars, and added math to determine et (total time for a 1/4 mile)... still needs function to that speed into distance per second

--- public/js/racing/cars/engine.ts
+++ public/js/racing/cars/engine.ts
@@ -1,17 +1,65 @@
 import { Car } from "./car";
 
-export class Engine {
+// -----
+export interface IEngine {
+    car: Car;
+    power: number;
+    torque: number;
+    type: string;
+
+    start(): void;
+    stop(): void;
+    getPower(): number;
+    getTorque(): number;
+}
+
+// -----
+// TQ measured in FT/LBS
+// Power measured in HP
+class Engine {
     public car: Car;
+    public power: number;
+    public torque: number;
+    public type: string;
 
-    constructor(
-        public type: string
-    ){}
-
-    start() {
+    public start() {
         console.log(`the is starting a ${this.type} engine`);
     }
 
-    stop(){
+    public stop() {
         console.log(`turning off the ${this.type} engine`);
     }
+
+    public getPower() {
+        return this.power;
+    }
+
+    public getToqrue() {
+        return this.torque;
+    }
+
+}
+
+// ------
+export class V8 extends Engine { 
+
+    constructor() {
+        super();
+        this.power = 400;
+        this.torque = 400;
+        this.type = 'V8';
+    }
+
+}
+
+// ------
+export class Electric extends Engine { 
+
+    constructor() {
+        super();
+        this.power = 400;
+        this.torque = 500;
+        this.type = 'Electric';
+    }
+
 }
--- public/js/racing/cars/formulaone.ts
+++ public/js/racing/cars/formulaone.ts
@@ -1,21 +1,22 @@
-
 import { Car } from "./car";
 
+
 export class FormulaOne extends Car {
+
     constructor(
-        public type: string,
+        public type:string,
         public model:string,
         public engine:Engine,
-        public tires: string,
-        public track: Track
-        
-    ){
+        public tires:string,
+        public track:Track ) {
+
         super(type, model, engine, tires, track);
         this.init();
     }
 
-    init(){
+    init() {
+
         this.getCarInfo();
     }   
-}
+
 }
