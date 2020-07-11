c2a8b6469cbc7efde4abdc5a580302f1f0a8e7a6
peb7268 <peb7268@gmail.com>
2020-07-11 09:25:17 -0600

    repo cleanup

--- tests/jest/spec/car.test.ts
+++ tests/jest/spec/car.test.ts
@@ -1,16 +1,16 @@
 
 
-const fs    = require('fs');
-const path  = require('path');
-const html  = fs.readFileSync(path.resolve(__dirname, '../mocks/dom/index.html'), 'utf8');
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, '../mocks/dom/index.html'), 'utf8');
 
 //Import our dependencies
-const Track         =   require('racing/track').Track;
-const Engine        =   require('racing/cars/engine').Engine;
-const Car           =   require('racing/cars/car').Car;
-const Audi          =   require('racing/cars/audi').Audi;
-const Tesla         =   require('racing/cars/tesla').Tesla;
-const FormulaOne    =   require('racing/cars/formulaone').FormulaOne;
+const Track = require('racing/track').Track;
+const Engine = require('racing/cars/engine').Engine;
+const Car = require('racing/cars/car').Car;
+const Audi = require('racing/cars/audi').Audi;
+const Tesla = require('racing/cars/tesla').Tesla;
+const FormulaOne = require('racing/cars/formulaone').FormulaOne;
 
 
 describe('Car', () => {
@@ -26,39 +26,12 @@ describe('Car', () => {
             'goodyear',
             new Track(document)
         );
-        
+
         expect(typeof car).toBe('object');
         expect(car.type).toBe('audi');
         expect(car.model).toBe('A4');
         expect(car.tires).toBe('goodyear');
         expect(typeof car.track).toBe('object');
     });
-
-    it('should create an electric car ', () => {
-        let car = new Car(
-            'tesla',
-            'models',
-            new Engine('electric'),
-            'goodyear',
-            new Track(document)
-        );
-        
-        expect(typeof car).toBe('object');
-        expect(car.type).toBe('tesla');
-        expect(car.model).toBe('models');
-        expect(car.engine.type).toBe('electric');
-    });
-
-    it('should have max speed', () => {
-        let car = new Car(
-            'audi',
-            'A4',
-            new Engine('V8'),
-            'goodyear',
-            new Track(document)
-        );
-
-        expect(car.maxSpeed).toBe(198); 
-    });    
 });
 
