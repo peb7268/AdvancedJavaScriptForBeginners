fec1a8c202da685c9a411a5689700598f06d062b
peb7268 <peb7268@gmail.com>
2020-07-11 19:58:08 -0600

    Testing tweaks

--- tests/cypress/integration/racing_app/start.spec.js
+++ tests/cypress/integration/racing_app/start.spec.js
@@ -1,21 +1,23 @@
 /// <reference types="cypress" />
 
-context('Actions', () => {
-    beforeEach(() => {
-      cy.visit('http://racingapp.com')
-    })
-  
-    describe('Track should have cars', () => {
-      it('should have a track', () => {
-        cy.get('#track').should('exist')
-      });
-  
-      it('should have cars on the page', () => {
-        cy.get('.car').should('exist')
-      });
+let headers = {};
 
-      it('should have exactly 2 cars', () => {
-        cy.get('.car').should('have.length', 2);
-      });
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://racingapp.com:8080')
+  })
+
+  describe('Track should have cars', () => {
+    it('should have a track', () => {
+      cy.get('#track').should('exist');
     });
+
+    it('should have cars on the page', () => {
+      cy.get('.car').should('exist')
+    });
+
+    it('should have exactly 2 cars', () => {
+      cy.get('.car').should('have.length', 2);
+    });
+  });
 });
